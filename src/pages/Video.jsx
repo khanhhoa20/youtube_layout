bad4fafbc964cf677b30cee660b6de32f5bb3079
khanhhoa20 <khanhhoa12t2@gmail.com>
2022-09-12 17:25:47 +0700

    finish

---
+++ src/pages/Video.jsx
@@ -0,0 +1,94 @@
+import styled from "styled-components"
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ThumbDownOffAltOutlinedIcon from "@mui/icons-material/ThumbDownOffAltOutlined";
+import ReplyOutlinedIcon from "@mui/icons-material/ReplyOutlined";
+import AddTaskOutlinedIcon from "@mui/icons-material/AddTaskOutlined";
+
+const Container = styled.div`
+    display: flex;
+    gap: 24px;
+`
+const Content = styled.div`
+flex: 5;
+`
+const Recommendation = styled.div`
+flex:2;
+`
+const VideoWrapper = styled.div`
+    
+`
+const Title = styled.h1`
+    font-size: 18px;
+    font-weight: 400;
+    margin-top: 20px;
+    margin-bottom: 10px;
+    color: ${({ theme }) => theme.text};
+
+`
+const Details = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+const Info = styled.span`
+     color: ${({ theme }) => theme.textSoft};
+`
+const Buttons = styled.div`
+    display: flex;
+    gap: 20px;
+    color: ${({ theme }) => theme.text};
+
+`
+const Button = styled.div`
+    display: flex;
+    align-items: center;
+    gap:5px;
+    cursor: pointer;
+`
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+const Video = () => {
+    return (
+        <Container>
+            <Content>
+                <VideoWrapper>
+                    <iframe
+                        width="100%"
+                        height="720"
+                        src="https://www.youtube.com/embed/k3Vfj-e1Ma4"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen
+                    ></iframe>
+                </VideoWrapper>
+                <Title>Test Video</Title>
+                <Details>
+                    <Info>
+                        10 views * Sep 12, 2022
+                    </Info>
+                    <Buttons>
+                        <Button>
+                            <ThumbUpOutlinedIcon /> 123
+                        </Button>
+                        <Button>
+                            <ThumbDownOffAltOutlinedIcon /> Dislike
+                        </Button>
+                        <Button>
+                            <ReplyOutlinedIcon /> Share
+                        </Button>
+                        <Button>
+                            <AddTaskOutlinedIcon /> Save
+                        </Button>
+                    </Buttons>
+                </Details>
+                <Hr />
+            </Content>
+            <Recommendation>recommendation</Recommendation>
+        </Container>
+    )
+}
+
+export default Video
